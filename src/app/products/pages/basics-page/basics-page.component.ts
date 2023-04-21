2d508c59b90621f4abfa673e80b6814039c9386a
Hugo Luis <hugoluis.system@gmail.com>
2023-04-21 00:12:42 -0500

    Date Pipe

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -10,7 +10,11 @@ export class BasicsPageComponent {
   public nameLower: string = 'hugo luis';
   
   public nameUpper: string = 'HUGO LUIS';
+
   public fullName: string = 'hUgO lUIs';
+
+  public customDate: Date = new Date();
+
 
 
 }
